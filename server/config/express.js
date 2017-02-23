36d76ca52def8531d4ab15e2900a4c205de66dfe
Shashank Tiwary <shashankt@axsystech-kol.com>
2017-02-23 18:26:28 +0530

    cors enabled for all sites now.

--- server/config/express.js
+++ server/config/express.js
@@ -13,6 +13,7 @@ var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
+var cors = require('cors');
 
 module.exports = function(app) {
   var env = app.get('env');
@@ -22,6 +23,7 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
+  app.use(cors());
   
   if ('production' === env) {
     app.use(morgan('dev'));
